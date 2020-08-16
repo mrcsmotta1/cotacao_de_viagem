6bbd71cafba503f01cefc6530724cfa44e868af8
Marcos Motta <mrcsmotta1@gmail.com>
2020-08-16 14:55:34 -0300

    Importado routes depois do body-parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import app from "./app";
 import bodyParser from "body-parser";
+import routes from "./routes/routes";
 
 const PORT = process.env.SERVER_PORT || 5000;
 const HOST = "0.0.0.0";
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+
 app.get("/", (req, res) => {
-  res.send("Hello Word!");
+  res.send("Hello World!");
 });
 
 app.listen(PORT, (HOST) => {
