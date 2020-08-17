f7f55dba99ecd5927200bc157879d355f8f21894
Marcos Motta <mrcsmotta1@gmail.com>
2020-08-17 11:45:29 -0300

    Alterado require da controller

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-import CotacaoController from "../../app/controllers/CotacaoController";
+import CotacaoController from "../app/controllers/CotacaoController";
 router.post("/quote/insert", CotacaoController.create);
 router.get("/quote/:from/:to", CotacaoController.list);
 
